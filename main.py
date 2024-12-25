07553496c30b48f016e0b3fd6d1e16ea30639fe1
WeiMa01 <54025805+WeiMa01@users.noreply.github.com>
2024-12-25 16:06:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ from transformers import AutoModelForCausalLM
 model = AutoModelForCausalLM.from_pretrained(
             args.model, torch_dtype=torch.float32, trust_remote_code=True
         ).to(args.device)
+
+
+class Hook:
+    """
+    Hook for get the activation data of each layers.
+    """
+
+    def __init__(self, module, backward=False):
+        if backward == False:
+            self.hook = module.register_forward_hook(self.hook_fn)
+        else:
+            self.hook = module.register_backward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        self.input = input
+        self.output = output
+
+    def close(self):
+        self.hook.remove()
